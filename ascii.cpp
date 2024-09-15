55bdb5fae1a5b50d274295175a1b709b17ba7beb
Vikash <massleader9336@gmail.com>
2024-09-15 13:35:35 +0530

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int av;
+    char ch;
+    cout<<"Enter a character to find its ASCII value:";
+    cin>>ch;
+    av=int(ch);
+    cout<<av;
+    getch();
+}
